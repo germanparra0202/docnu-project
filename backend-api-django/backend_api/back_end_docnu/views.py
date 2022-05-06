92d839490d309d3f1e4b06ae0f635621151de722
German Parra <germanparra@Germans-MacBook-Pro-3.local>
2022-05-06 13:43:39 -0700

    initial commit

---
+++ backend-api-django/backend_api/back_end_docnu/views.py
@@ -0,0 +1,7 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response 
+from rest_framework import status
+from datetime import datetime
+from django.shortcuts import render
+
+# Create your views here.
